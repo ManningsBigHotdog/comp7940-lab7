0232827e6b105c4d8ccef8e3a33b73c6653a141b
Tim Tong <tcpgwin1>
2024-03-06 14:28:29 +0800

    apply serect

--- ChatGPT_HKBU.py
+++ ChatGPT_HKBU.py
@@ -1,3 +1,4 @@
+import os
 import configparser
 import requests
 
@@ -12,7 +13,8 @@ class HKBU_ChatGPT():
     def submit(self, message):   
         conversation = [{"role": "user", "content": message}]
         url = (self.config['CHATGPT']['BASICURL']) + "/deployments/" + (self.config['CHATGPT']['MODELNAME']) + "/chat/completions/?api-version=" + (self.config['CHATGPT']['APIVERSION'])
-        headers = { 'Content-Type': 'application/json', 'api-key': (self.config['CHATGPT']['ACCESS_TOKEN']) }
+        headers = { 'Content-Type': 'application/json', 'api-key': (os.environ.get('GPT_ACCESS_TOKEN')) }
+        # headers = { 'Content-Type': 'application/json', 'api-key': (self.config['CHATGPT']['ACCESS_TOKEN']) }
         payload = { 'messages': conversation }
         response = requests.post(url, json=payload, headers=headers)
         if response.status_code == 200:
